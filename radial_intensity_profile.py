8607bed8c9b1133d271a1e1d2fbdd0777f3fab92
Abhiram <abhivinjav@gmail.com>
2025-07-24 16:11:41 +0530

    Update radial_intensity_profile.py

--- radial_intensity_profile.py
+++ radial_intensity_profile.py
@@ -46,8 +46,8 @@ for df in water_dfs:
         sns.lineplot(data=df, x='X_Fit:_Gaussian', y='Fit:_Gaussian', color='blue', linewidth=0.75, label='Gaussian Fit')
     else:
         plt.figure()
-        sns.lineplot(data=df, x='Radius_[pixels]', y='Normalized_Integrated_Intensity', color='black', linewidth=1)
-        sns.lineplot(data=df, x='X_Fit:_Gaussian', y='Fit:_Gaussian', color='blue', linewidth=0.75)
+        sns.lineplot(data=df, x='Radius_[pixels]', y='Normalized_Integrated_Intensity', color='black', linewidth=1, label='Gaussian Fit')
+        sns.lineplot(data=df, x='X_Fit:_Gaussian', y='Fit:_Gaussian', color='blue', linewidth=0.75, label='Gaussian Fit')
 
     plt.title(fr'Radial Intensity Profile {i} $I_{i}(r)$')
     plt.xlabel('Radius (mm)')
